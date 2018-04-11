aac95c057dac7f29907ad4aa4d4817181b441ae8
Jimmy Huang <racketprogram@gmail.com>
2018-04-12 02:08:49 +0800

    Add input example code

--- src/reducers/home.js
+++ src/reducers/home.js
@@ -1,10 +1,14 @@
-const home = (state = {}, action) => {
+const home = (state = {userName:　''}, action) => {
     switch (action.type) {
         case "INIT":
             return {
                 ...state,
                 init: action.init
             };
+        case "INPUT_USER_NAMES":
+            return Object.assign({}, state, {
+                userName: action.userName
+            });
         default:
             return state;
     }
